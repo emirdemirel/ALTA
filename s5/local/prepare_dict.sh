823be6940554c03db96a340debd3eccc5ccfd0b0
Emir Demirel <ed308@braintree.eecs.qmul.ac.uk>
2020-11-19 18:12:08 +0000

    modified this script for easy execution

--- s5/local/prepare_dict.sh
+++ s5/local/prepare_dict.sh
@@ -27,12 +27,7 @@ mkdir -p data
 dir=data/local/dict
 mkdir -p $dir
 
-echo "$0: Getting CMU dictionary"
-if [ ! -f $dir/cmudict.done ]; then
-  [ -d $dir/cmudict ] && rm -rf $dir/cmudict
-  svn co https://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict $dir/cmudict
-  touch $dir/cmudict.done
-fi
+cp -r conf/dict/* $dir/
 
 echo "$0: Preparing files in $dir"
 # Silence phones
@@ -40,34 +35,18 @@ for w in SIL SPN; do echo $w; done > $dir/silence_phones.txt
 echo SIL > $dir/optional_silence.txt
 
 
-# For this setup we're discarding stress.
-cat $dir/cmudict/cmudict-0.7b.symbols | \
-  perl -ne 's:[0-9]::g; s:\r::; print lc($_)' | \
-  tr a-z A-Z | \
-  sort -u > $dir/nonsilence_phones.txt
-
-# An extra question will be added by including the silence phones in one class.
-paste -d ' ' -s $dir/silence_phones.txt > $dir/extra_questions.txt
-
-
-grep -v ';;;' $dir/cmudict/cmudict-0.7b |\
-  uconv -f latin1 -t utf-8 -x Any-Lower |\
-  perl -ne 's:(\S+)\(\d+\) :$1 :; s:  : :; print;' |\
-  perl -ne '@F = split " ",$_,2; $F[1] =~ s/[0-9]//g; print "$F[0] $F[1]";' \
-  > $dir/lexicon1_raw_nosil.txt || exit 1;
-
 
 # Add prons for laughter, noise, oov
 for w in `grep -v sil $dir/silence_phones.txt`; do
   echo "[$w] $w"
-done | cat - $dir/lexicon1_raw_nosil.txt > $dir/lexicon2_raw.txt || exit 1;
+done | cat - $dir/lexicon_raw.txt > $dir/lexicon2_raw.txt || exit 1;
 
 
 # we keep all words from the cmudict in the lexicon
 # might reduce OOV rate on dev and test
 cat $dir/lexicon2_raw.txt  \
-   <( echo "mm m"
-      echo "<unk> spn" \
+   <( echo "MM M"
+      echo "<UNK> SPN" \
     )  | sed 's/[\t ]/\t/' | tr a-z A-Z  | sort -u > $dir/iv_lexicon.txt
 
 
@@ -108,30 +87,17 @@ echo "*Highest-count OOVs (including fragments) are:"
 head -n 10 $dir/oov_counts.txt
 echo "*Highest-count OOVs (excluding fragments) are:"
 grep -v -E '^-|-$' $dir/oov_counts.txt | head -n 10 || true
+tail -n +4 $dir/oov_counts.txt | awk '{print $2}' > $dir/oov_words.txt
+
+echo "Extend lexicon using a G2P model."
+./steps/dict/apply_g2p_phonetisaurus.sh --nbest 3 \
+    data/local/dict/oov_words.txt conf/g2p data/local/dict/
+cut -d$'\t' -f1,3 data/local/dict/lexicon.lex > data/local/dict/lex
+sed -e 's/\t/ /g' data/local/dict/lex > data/local/dict/oov_lexicon.txt
+cat data/local/dict/oov_lexicon.txt data/local/dict/lexicon_raw.txt | sort -u > data/local/dict/lexicon.txt
+sed -e 's/ / 1.0\t/' data/local/dict/lexicon.txt > data/local/dict/lexiconp.txt
 
 
-echo "*Training a G2P and generating missing pronunciations"
-mkdir -p $dir/g2p/
-
-if [ -e $dir/g2p/g2p.fst ]
-then
-    echo "$0: Phonetisaurus exist. $dir/g2p/g2p.fst will be used"
-else
-    phonetisaurus-align --input=$dir/iv_lexicon.txt --ofile=$dir/g2p/aligned_lexicon.corpus
-    ngram-count -order 4 -kn-modify-counts-at-end -ukndiscount\
-      -gt1min 0 -gt2min 0 -gt3min 0 -gt4min 0 \
-      -text $dir/g2p/aligned_lexicon.corpus -lm $dir/g2p/aligned_lexicon.arpa
-    phonetisaurus-arpa2wfst --lm=$dir/g2p/aligned_lexicon.arpa --ofile=$dir/g2p/g2p.fst
-fi
-
-awk '{print $2}' $dir/oov_counts.txt > $dir/oov_words.txt
-phonetisaurus-apply --nbest 2 --model $dir/g2p/g2p.fst --thresh 5 --accumulate \
-  --word_list $dir/oov_words.txt > $dir/oov_lexicon.txt
-
-
-## We join pronunciation with the selected words to create lexicon.txt
-cat $dir/oov_lexicon.txt $dir/iv_lexicon.txt | sort -u > $dir/lexicon1_plus_g2p.txt
-join $dir/lexicon1_plus_g2p.txt $dir/word_list_sorted > $dir/lexicon.txt
 
 echo "<UNK> SPN" >> $dir/lexicon.txt
 
